6f1106dbad9fbf0ea1995324d90f8aea0e66c4be
Bashitha267 <nimeshspc2k17@gmail.com>
2026-07-24 00:00:30 +0530

    ff

--- src/data/speakersData.js
+++ src/data/speakersData.js
@@ -3,30 +3,30 @@
 export const speakersData = [
   {
     id: 'naresh-shanmugaraj',
-    name: 'Mr. Naresh Shanmugaraj',
+    name: 'Mr.Naresh Shanmugaraj',
     position: 'Founder and CEO of Cogntix (Pvt) Ltd',
     topic: 'AI-powered Requirement Engineering and Product Discovery',
     image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784816780/Naresh.jpg__1_-removebg-preview_jspofa.png'
   },
   {
     id: 'jude-sheron',
-    name: 'Mr. Jude Selvakumar Anto Sheron',
+    name: 'Mr.Jude Selvakumar Anto Sheron',
     position: 'Senior Software Engineer Cogntix (Pvt) Ltd',
     topic: 'AI-Assisted Development and Code Generation',
     image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784816926/Sheron.jpg_1_v8vegx.jpg'
   },
   {
     id: 'susara-jayaweera',
-    name: 'Eng. Susara Jayaweera Patabendige',
+    name: 'Mr.Susara Jayaweera Patabendige',
     position: 'Associate Tech Lead – AI/ML | VitalHub Innovations Lab',
     topic: 'AI for the enterprise & the AI native engineer',
-    image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784816373/image001_cdmp1n.png'
+    image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784816783/WhatsApp_Image_2026-07-18_at_23.19.54-removebg-preview_omz6ey.png'
   },
   {
     id: 'gnanakeethan-balasubramaniam',
-    name: 'Mr. Gnanakeethan Balasubramaniam',
+    name: 'Mr.Gnanakeethan Balasubramaniam',
     position: 'Founder and Lead Architect of Cloud Parallax (Pvt) Ltd',
     topic: 'AI-driven DevOps, monitoring, and predictive maintenance',
-    image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784817786/images_17_jkjmlt.jpg'
+    image: 'https://res.cloudinary.com/dnfbik3if/image/upload/v1784831366/WhatsApp_Image_2026-07-18_at_23.19.58-removebg-preview_1_c4ged3.png'
   }
 ];
